12fc4a015f62e631fc880fcb35884a2955c9b8a8
mgkim9704 <mgkim9704@kaist.ac.kr>
2016-12-20 19:48:24 +0900

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ nx.onload = function() {
 	// some code using data.press, data.x, and data.y
 		console.log("click");
 		gainNode1.gain.exponentialRampToValueAtTime(0.01, 10);
+		source1.stop(audioContext.currentTime+10);
 		source2.start();
 		gainNode2.gain.exponentialRampToValueAtTime(1, 10);
 		console.log(audioContext.currentTime);
