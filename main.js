1920b6956c925f2e3900e5bdeb372ef39d1fa13e
mgkim9704 <mgkim9704@kaist.ac.kr>
2016-12-22 02:37:42 +0900

    Update main.js

--- main.js
+++ main.js
@@ -106,7 +106,6 @@ function loadSampleAudio() {
 	
 	source2 = audioContext.createBufferSource();
 	gainNode2 = audioContext.createGain();
-	gainNode2.gain.value=0;
 	analyser2 = audioContext.createAnalyser();
 	analyser2.smoothingTimeConstant = 0.1;
 	analyser2.fftSize = 1024;
